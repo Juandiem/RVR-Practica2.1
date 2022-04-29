ee63635f455b402c63010738cd426e08f2f08b05
Oxelion <juandiem@ucm.es>
2022-04-29 11:32:08 +0200

    Ejercicio 2 hecho

--- Practica2.2/serializacion/ejercicio1.cc
+++ Practica2.2/serializacion/ejercicio1.cc
@@ -12,7 +12,7 @@
 class Jugador: public Serializable
 {
 public:
-    Jugador(const char * _n, int16_t _x, int16_t _y):x(_x),y(_y)
+    Jugador(const char * _n, int16_t _x, int16_t _y):pos_x(_x), pos_y(_y)
     {
         strncpy(name, _n, 80);
     };
@@ -21,34 +21,63 @@ public:
 
     void to_bin()
     {
-        //
+        alloc_data(sizeof(char) * MAX_NAME + sizeof(int16_t) * 2); //name + pos_x + pos_y
+        char* tmp = _data + sizeof(int32_t);
+        memcpy(tmp, (void*)name,sizeof(char) * MAX_NAME);
+        tmp += sizeof(char) * MAX_NAME;
+        memcpy(tmp, &pos_x,sizeof(int16_t));
+        tmp += sizeof(int16_t);
+        memcpy(tmp, &pos_y,sizeof(int16_t));
     }
 
     int from_bin(char * data)
     {
-        //
+        char * tmp = data + sizeof(int32_t);
+        memcpy(name, tmp, MAX_NAME);
+        tmp += MAX_NAME;
+        memcpy(&pos_x, tmp, sizeof(int16_t));
+        tmp += sizeof(int16_t);
+        memcpy(&pos_y, tmp, sizeof(int16_t));
+
         return 0;
     }
 
 
 public:
-    char name[80];
 
-    int16_t x;
-    int16_t y;
+    static const size_t MAX_NAME = 80;
+    char name[MAX_NAME];
+
+    int16_t pos_x;
+    int16_t pos_y;
 };
 
 int main(int argc, char **argv)
 {
-    Jugador one_r("", 0, 0);
+    Jugador one_r("-", 0, 0);
     Jugador one_w("Player_ONE", 123, 987);
 
-    // 1. Serializar el objeto one_w
-    // 2. Escribir la serialización en un fichero
-    // 3. Leer el fichero
-    // 4. "Deserializar" en one_r
-    // 5. Mostrar el contenido de one_r
+    //Serializar y escribir one_w en un fichero
+    //Leer el fichero en un buffer y "deserializar" en one_r
+    //Mostrar el contenido one_r
+
+    int file = open("one_w.txt", O_RDWR | O_TRUNC | O_CREAT);
+
+    if(file < 0) return -1;
+
+    one_w.to_bin(); //serialize player
+
+    write(file, one_w.data(), one_w.size());
+
+
+    read(file, one_w.data(), one_w.size());
+
+    close(file);
+
+    one_r.from_bin(one_w.data());
+
+    std::cout << "Player name: " << one_r.name << " Pos_x: " << one_r.pos_x << " Pos_y: " << one_r.pos_y << std::endl;
+
 
     return 0;
 }
-
